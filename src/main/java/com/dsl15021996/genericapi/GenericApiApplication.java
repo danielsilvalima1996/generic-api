622cc013ad313acc84d64efbd335183ee1e07a25
danielsilvalima1996 <dsl15021996@gmail.com>
2022-02-22 13:45:27 -0300

    adicionado arquivos projeto

---
+++ src/main/java/com/dsl15021996/genericapi/GenericApiApplication.java
@@ -0,0 +1,13 @@
+package com.dsl15021996.genericapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsl15021996/genericapi/GenericApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsl15021996.genericapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
